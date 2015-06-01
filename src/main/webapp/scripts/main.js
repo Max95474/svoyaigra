2095604cf59c7c93e7149c57b66f1552a7104759
simm25 <sim250495@gmail.com>
2015-06-01 22:09:15 +0300

    progress bar and text fade

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -14,10 +14,23 @@ $(document).ready(function(){
 			}
 			$(this).find('ul:first').stop(true, true).slideToggle();
 		});
+
 		$(".package_title").click(function() {
 			$("#quest_topics").hide();
 			$("#quest_game").load("quest.html", function() {
 				$(this).fadeIn(500);
+					 $.fn.animate_Text = function() {
+					  var string = this.text();
+					  return this.each(function(){
+					   var $this = $(this);
+					   $this.html(string.replace(/./g, '<span class="new">$&</span>'));
+					   $this.find('span.new').each(function(i, el){
+					    setTimeout(function(){ $(el).addClass('div_opacity'); }, 40 * i);
+					   });
+					  });
+					 };
+			 $('#quest_title').show();
+			 $('#quest_title').animate_Text();
 				$("#quest_button").click(function() {
 					$("#reply_field").load("reply_field.html", function(){
 						$("#quest_button").hide();
@@ -25,7 +38,6 @@ $(document).ready(function(){
 							$("#reply_field_show").hide();
 							$("#quest_button").show();
 						});
-						
 					});
 				});
 				$("#icon_a").click(function() {
@@ -36,11 +48,3 @@ $(document).ready(function(){
 		});
 	});
 });
-
-
-
-
-
-
-
-	
